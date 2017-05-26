f1317b9302167b01a7eeadc3a59c32810b0359b7
nambihawkins <nambihawkins@fullsail.edu>
2017-05-25 22:40:23 -0400

    prepare database information

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      \DB::table('users')->delete();
+      User::create([
+        'firstname' => 'nambi',
+        'lastname' => 'hawkins',
+        'username' => 'nomnom',
+        'email' => 'nambi.hawkins@gmail.com',
+        'password' => bcrypt('cheese')
+      ]);
+    }
+}
